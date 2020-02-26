73a082b39e0191d13a96e6b448d7ce6099a5ef23
JiM-W <550116990@qq.com>
2020-02-26 16:09:43 +0800

    更新'

---
+++ release.sh
@@ -0,0 +1,8 @@
+set -e
+GIT_PAGE='git@github.com:beatles-chameleon/light-ui.github.io.git'
+PROJECT_NAME="light-ui.github.io"
+if [ ! d "./$PROJECT_NAME"];then
+git clone $GIT_PAGE && gitbook install && gitbook build && mv -f _book/* ./light-ui.github.io/
+else
+gitbook install && gitbook build && mv -f _book/* ./light-ui.github.io/
+fi
